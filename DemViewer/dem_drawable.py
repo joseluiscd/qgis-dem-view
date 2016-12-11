2aa1d09fce99d06b1dd900c6076e6db565a981c6
José Luis C.D <jlcd0003@red.ujaen.es>
2016-12-11 19:53:10 +0100

    Draw DEM

--- DemViewer/dem_drawable.py
+++ DemViewer/dem_drawable.py
@@ -1,58 +1,100 @@
+#The future is a great place!! But Python3 has been out there for a while...
+from __future__ import division
+
 from OpenGL import GL
+from qgis.core import *
+import numpy
+
+def normalize(n):
+    lens = numpy.sqrt(n[:,0]**2 + n[:,1]**2 + n[:,2]**2 )
+    n[:,0] /= lens
+    n[:,1] /= lens
+    n[:,2] /= lens
+
+    return n
 
 class DemDrawable:
     def __init__(self, heightData, colorData, divisions):
         self.divisions = divisions
 
-        self.heightData = heightData.block(
-            0, heightData.extent(), 1, 1
-        )
-
-        print(heightData.extent())
-
+        self.heightData = heightData
+        self.extent = heightData.extent()
 
         self.buildMesh()
 
     def buildMesh(self):
         vertices = []
+        indices = []
 
         w = self.divisions
         h = self.divisions
+        quad = 0
         for x in range(w-1):
             for y in range(h-1):
-                z1 = 0#self.heightData.value(y, x)
-                z2 = 0#self.heightData.value(y, x+1)
-                z3 = 0#self.heightData.value(y+1, x)
-                z4 = 0#self.heightData.value(y+1, x+1)
+                print("Punto:",(x,y))
+                z1 = self.getValue(x/w, y/h)
+                z2 = self.getValue((x+1)/w, y/h)
+                z3 = self.getValue(x/w, (y+1)/h)
+                z4 = self.getValue((x+1)/w, (y+1)/h)
 
-                v1 = (x//w, y//h, z1)
-                v2 = (x+1//w, y//h, z2)
-                v3 = (x//w, y+1//h, z3)
-                v4 = (x+1//w, y+1//h, z4)
+                v1 = (x/w, z1, y/h)
+                v2 = ((x+1)/w, z2, y/h)
+                v3 = (x/w, z3, (y+1)/h)
+                v4 = ((x+1)/w, z4, (y+1)/h)
 
                 vertices.append(v1)
                 vertices.append(v2)
                 vertices.append(v3)
-
-                vertices.append(v3)
-                vertices.append(v2)
                 vertices.append(v4)
 
+                indices.append((quad+1, quad+0, quad+2))
+                indices.append((quad+1, quad+2, quad+3))
 
-        self.vertices = vertices
+                quad += 4
 
+        vertices = numpy.array(vertices)
+        indices = numpy.array(indices)
+        normals = numpy.zeros(vertices.shape, dtype=vertices.dtype)
+        print(indices)
+        tris = vertices[indices]
+
+        n = numpy.cross(tris[::,1]-tris[::,0], tris[::,2]-tris[::,0])
+
+        n = normalize(n)
+
+        normals[indices[:,0]] += n
+        normals[indices[:,1]] += n
+        normals[indices[:,2]] += n
+
+        self.vertices = vertices[indices]
+        self.normals = normalize(normals[indices])
+
+
+
+    def getValue(self, x, y):
+        """
+        x and y between 0 and 1
+        """
+
+        realX = self.extent.xMinimum() + (x*(self.extent.xMaximum()-self.extent.xMinimum()))
+        realY = self.extent.yMinimum() + (y*(self.extent.yMaximum()-self.extent.yMinimum()))
+
+        print((realX, realY))
+        val = self.heightData.identify(QgsPoint(realX, realY), QgsRaster.IdentifyFormatValue)
+        if val.isValid():
+            print(val.results())
+            return val.results()[1]
 
     def draw(self):
-        GL.glMaterialfv(GL.GL_FRONT_AND_BACK, GL.GL_AMBIENT, (0.1,0.1,0.1))
-        GL.glMaterialfv(GL.GL_FRONT_AND_BACK, GL.GL_DIFFUSE, (0.6,0.6,0.6))
+        GL.glMaterialfv(GL.GL_FRONT_AND_BACK, GL.GL_AMBIENT, (0.1,0.1,0.1,1))
+        GL.glMaterialfv(GL.GL_FRONT_AND_BACK, GL.GL_DIFFUSE, (1,1,1,1))
+        GL.glMaterialfv(GL.GL_FRONT_AND_BACK, GL.GL_SPECULAR, (0.6,0.6,0.6,1))
+        GL.glMaterialf(GL.GL_FRONT_AND_BACK, GL.GL_SHININESS, 20)
+        GL.glMaterialfv(GL.GL_FRONT_AND_BACK, GL.GL_EMISSION, (0,0,0,0));
 
-        GL.glBegin(GL.GL_TRIANGLES)
-        GL.glColor(1,1,1)
-        for v in self.vertices:
-            GL.glVertex(*v)
-            print(v)
-        #GL.glVertex(0, 0, 0)
-        #GL.glVertex(1, 0, 0)
-        #GL.glVertex(1, 1, 0)
 
-        GL.glEnd()
+        GL.glEnableClientState(GL.GL_VERTEX_ARRAY)
+        GL.glEnableClientState(GL.GL_NORMAL_ARRAY)
+        GL.glVertexPointer(3, GL.GL_FLOAT, 0, self.vertices)
+        GL.glNormalPointer(GL.GL_FLOAT, 0, self.normals)
+        GL.glDrawArrays(GL.GL_TRIANGLES, 0, len(self.vertices)*3)
--- DemViewer/dem_render.py
+++ DemViewer/dem_render.py
@@ -1,5 +1,5 @@
 import PyQt4
-from PyQt4 import QtGui, QtOpenGL
+from PyQt4 import QtCore, QtGui, QtOpenGL
 
 from OpenGL import GL, GLU
 
@@ -8,34 +8,71 @@ class DemRenderer(QtOpenGL.QGLWidget):
         super(QtOpenGL.QGLWidget, self).__init__(parent)
         self.dem = dem
 
+        self.xRot = 0
+        self.yRot = 0
+        self.zRot = 0
+
+        self.lastPos = QtCore.QPoint()
+
     def initializeGL(self):
-        GL.glClearColor(0.1,0.1,0.1,1)
-        GL.glShadeModel(GL.GL_FLAT)
+        GL.glClearColor(0.8,0.8,1,1)
+        GL.glShadeModel(GL.GL_SMOOTH)
+
         GL.glEnable(GL.GL_DEPTH_TEST)
-        GL.glEnable(GL.GL_AUTO_NORMALS)
+        GL.glEnable(GL.GL_AUTO_NORMAL)
         GL.glEnable(GL.GL_NORMALIZE)
         GL.glEnable(GL.GL_LIGHTING)
+        GL.glEnable(GL.GL_CULL_FACE)
+
+        GL.glEnable(GL.GL_LIGHT0)
+        GL.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT, (0.2, 0.2, 0.2, 1))
+        GL.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, (0.5, 0.5, 0.5, 1))
+        GL.glLightfv(GL.GL_LIGHT0, GL.GL_SPECULAR, (0.3, 0.3, 0.3, 1))
+        GL.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, (-3, 3, -3, 1))
+
 
 
 
     def resizeGL(self, width, height):
         side = min(width, height)
-        if side < 0:
+        if side <= 0:
             return
 
-        GL.glViewport((width - side) / 2, (height - side) / 2, side, side)
+        GL.glViewport(0, 0, width, height)
+
         GL.glMatrixMode(GL.GL_PROJECTION)
         GL.glLoadIdentity()
-        #GL.glOrtho(-1, +1, +1, -1, 0.1, 15.0)
-        GLU.gluPerspective(40, 1, 0.1, 15)
+        GLU.gluPerspective(40, width//height, 0.1, 100)
         GL.glMatrixMode(GL.GL_MODELVIEW)
 
     def paintGL(self):
         GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
         GL.glLoadIdentity()
-        GLU.gluLookAt(2,2,2,0,0,0,0,1,0)
+        GLU.gluLookAt(2,2,2,0.5,0,0.5,0,1,0)
 
+        GL.glRotate(self.xRot, 1, 0, 0)
+        GL.glRotate(self.yRot, 0, 1, 0)
+        GL.glRotate(self.zRot, 0, 0, 1)
+        GL.glTranslate(-0.5, 0, -0.5)
+        GL.glScale(1, 0.001, 1)
         self.dem.draw()
+
+    def mousePressEvent(self, event):
+        self.lastPos = event.pos()
+
+    def mouseMoveEvent(self, event):
+        dx = event.x() - self.lastPos.x()
+        dy = event.y() - self.lastPos.y()
+
+        if event.buttons() & QtCore.Qt.LeftButton:
+            self.xRot += dy
+            self.yRot += dx
+        elif event.buttons() & QtCore.Qt.RightButton:
+            self.xRot += dy
+            self.zRot += dx
+
+        self.lastPos = event.pos()
+        self.updateGL()
 
 
 class DemViewerWindow(QtGui.QWidget):
